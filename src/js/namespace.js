a2097adac1f7bf5d03916798b78a84de2517e5f7
hunyong-song <hunyong.song@nhn.com>
2016-05-29 15:31:09 +0900

    feat(jsSrc) - namespace

---
+++ src/js/namespace.js
@@ -0,0 +1,12 @@
+/**
+ * NTS 네임스페이스
+ * @namespace
+ * @author hunyong.song
+ * @since 2016.05.22
+ */
+var nts = window.nts || {
+        model: {},
+        view: {},
+        controller: {},
+        helper: {}
+    };
